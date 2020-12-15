1fd0980d2a3a60da9a66048de8fbbeadc00f4585
303StA <73601416+303StA@users.noreply.github.com>
2020-12-15 11:58:47 +0000

    Update Individual.java

--- src/entities/Individual.java
+++ src/entities/Individual.java
@@ -1,11 +1,11 @@
-package entities;
+package src.entities;
 
 import java.util.LinkedList;
 import java.util.List;
 
 public class Individual {
-    private static final Integer chromosomesNumber = 2;
-    private List<Chromosome> chromosomes;
+    public Integer chromosomesNumber = 1;
+    List<Chromosome> chromosomes;
 
     public Individual() {
         chromosomes = new LinkedList<>();
@@ -13,12 +13,29 @@ public class Individual {
             chromosomes.add(new Chromosome());
         }
     }
+    
+    //My code
+    public Individual(List<Chromosome> list){
+        this.chromosomes = list;
+    }
+    
 
+    public List<Chromosome> getChromosomes() {
+        return chromosomes;
+    }
+
+    public void setChromosomes(List<Chromosome> chromosomes) {
+        this.chromosomes = chromosomes;
+    }
+
+    
+    
     public String toString() {
         StringBuilder result = new StringBuilder();
         for (Chromosome c : chromosomes) {
             result.append(c.toString());
             result.append("\n");
+            //System.out.println("chromosome : "+ c.toString());
         }
         return result.toString();
     }
