301ffc4da96df9b34d7157cf90798100fee0349a
303StA <73601416+303StA@users.noreply.github.com>
2020-12-15 11:55:06 +0000

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -1,8 +1,36 @@
-import entities.Population;
+package src;
+
+import src.entities.Base;
+import src.entities.Individual;
+import src.entities.Population;
 
 public class Runner {
-	public static void main(String[] args) {
-		Population p = new Population(10);
-		System.out.println(p.toString());
-	}
+    
+    static Algorithm algo = new Algorithm(new Population(10));
+    static Individual div1, div2, offspring;
+    static Base first_base, second_base;
+    
+    public static void main(String[] args) {
+        String match = "AA";
+        String result = run();
+        while(!result.equals(match)){
+            result = run();
+        }
+        
+        System.out.println("We have found a match\n");
+        System.out.println("Generation: " + algo.getGenerationCount());
+        System.out.println("Genes: " + offspring.getChromosomes().get(0).getGenes().toString());
+            
+    }
+    
+    public static String run(){
+        div1 = algo.selection();
+        div2 = algo.selection();
+        offspring = algo.crossover(div1,div2);
+        first_base = offspring.getChromosomes().get(0).getGenes().get(0).getCode().get(0);
+        second_base = offspring.getChromosomes().get(0).getGenes().get(0).getCode().get(1);
+        String combo = first_base.toString() + second_base.toString();
+        return combo;
+    }
+   
 }
