21553f2738bfbf6ead985ac0c81a925f6ba0c278
Shamsiddin Nabiev <shamsiddin@live.ru>
2020-12-16 13:39:27 +0000

    added gene binary encoding & random generator

--- src/ent/Individual.java
+++ src/ent/Individual.java
@@ -1,6 +1,17 @@
 package ent;
+
 import java.util.List;
 
 public abstract class Individual {
     protected List<Character> genes;
+
+    public String toString() {
+        StringBuffer result = new StringBuffer();
+        result.append("[");
+        for (Character c : genes) {
+            result.append(c);
+        }
+        result.append("]");
+        return result.toString();
+    }
 }
--- src/ent/IndividualX.java
+++ src/ent/IndividualX.java
@@ -6,5 +6,13 @@ public class IndividualX extends Individual {
     public IndividualX() {
         // genes are decoded as bits
         genes = new ArrayList<Character>();
+        for (int i = 0; i < 10; i++) {
+            if (Math.random() > 0.5) {
+                genes.add('0');
+            } else {
+                genes.add('1');
+
+            }
+        }
     }
 }
