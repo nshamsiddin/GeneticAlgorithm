b7d4df47b089e7c1e29711748a28b251709962e4
Shamsiddin Nabiev <shamsiddin@live.ru>
2020-12-16 17:18:45 +0000

    initial commit

---
+++ src/factories/crossover/Uniform.java
@@ -0,0 +1,33 @@
+package factories.crossover;
+
+import java.util.List;
+
+import ent.individual.Individual;
+
+public class Uniform implements Crossover {
+
+    // Abstract Factory's Factory method is a thread-safe Singleton class
+    private static final Uniform instance = new Uniform();
+
+    // private constructor to avoid other classes to use constructor
+    private Uniform() {
+    }
+
+    /**
+     * @return Uniform
+     */
+    public static Uniform getInstance() {
+        return instance;
+    }
+
+    /**
+     * @param individuals
+     * @return List<Individual>
+     */
+    @Override
+    public List<Individual> doCrossover(List<Individual> individuals) {
+        System.out.println("Performing uniform crossover...");
+        return individuals;
+    }
+
+}
